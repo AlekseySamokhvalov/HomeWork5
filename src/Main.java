df3f565be62170e4464b2f349a05193eea3be9c6
AlekseySamokhvalov <6560@mail.ru>
2023-06-08 10:48:32 +0300

    Решение задачи 2 через case/

--- src/Main.java
+++ src/Main.java
@@ -36,6 +36,52 @@ public class Main {
     public static void task2 () {
         System.out.println("Задача 2");
         // Пишем код для задачи 2
+        int clientOS = 1; // 0 для iOS, 1 для Android
+        int clientDeviceYear = 2015; // год выпуска клиентского устройства
+/*
+        if (clientDeviceYear < 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if (clientOS == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Неверное значение переменной clientOS");
+            }
+        } else {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else if (clientOS == 1) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Неверное значение переменной clientOS");
+            }
+        }
+ */
+        if (clientDeviceYear < 2015) {
+            switch (clientOS) {
+                case 0:
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                    break;
+                case 1:
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                    break;
+                default:
+                    System.out.println("Неверное значение переменной clientOS");
+                    break;
+            }
+        } else {
+            switch (clientOS) {
+                case 0:
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                    break;
+                case 1:
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                    break;
+                default:
+                    System.out.println("Неверное значение переменной clientOS");
+                    break;
+            }
+        }
     }
     public static void task3 () {
         System.out.println("Задача 3");
